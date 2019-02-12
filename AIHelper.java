260cc232e7350f2cc27f996ae094f9bae450d374
Joshua Karol <josh@themistechnology.com>
2019-02-12 17:28:04 -0500

    First commit

---
+++ AIHelper.java
@@ -0,0 +1,27 @@
+class AIHelper{
+
+    /**
+    * Generates an array of all playable moves from a given state
+    *
+    * @param  b     The current state of the board
+    * @return   An array  of possible boards
+    */
+    public static Board[] genAllMoves(Board b){
+        return null;
+    }
+
+    /**
+    * Calculates next best move for the opponent using minimax
+    *
+    * @param  level   The number of levels deep to search in the tree
+    * @param  c   The person who is playing ('X' or 'O')
+    * @param  alpha   Best score of the maximizer
+    * @param  beta   Best score of the minimizer
+    * @param  b   The board being searched
+
+    * @return         An array  of possible boards
+    */
+    public static int[] minimax(int level, char c, int alpha, int beta, Board b){
+            return null;
+    }
+}
---
+++ Board.java
@@ -0,0 +1,83 @@
+class Board{
+    char[][] positions = new char[6][7]; // Represents pieces on the board
+    int moves = 0; // Counts number of moves played
+    int lastMove = 0; // Keeps track of the last move played
+
+    /**
+    * Converts the board into a user friendly string
+    *
+    * @return   The board represented as a string
+    */
+    public String toString(){
+        return null;
+    }
+
+    /**
+    * Checks to see if a piece can be played in a given column
+    *
+    * @param  col   The column being checked
+    * @return   Whether or not the piece can be placed there
+    */
+    public boolean isColPlayable(int col){
+        return true;
+    }
+
+    /**
+    * Plays a piece in the given column
+    *
+    * @param  c    The person placing the piece ('X' or 'O')
+    * @param  col   The column being played
+    */
+    public void playPiece(char c,int col){
+       return;
+    }
+
+    /**
+    * Checks to see if a player has won the game
+    *
+    * @param  c     The person who may or may not have won
+    * @return   Whether or not they have won
+    */
+    public boolean checkWin(char c){
+        return false;
+    }
+
+    /**
+    * Assigns a score to the current state of the board for use in minimax
+    *
+    * @return   The score of the board
+    */
+    public int scoreBoard(){
+        return 0;
+    }
+
+    /**
+    * Tells us whether or not the game is a draw 
+    *
+    * @return   Whether or not the game is a draw 
+    */
+    public boolean isDraw(){
+        return false;
+    }
+}
+
+/* 
+
+Example Board
+
+  1   2   3   4   5   6   7
++---+---+---+---+---+---+---+
+|   |   |   |   |   |   |   |
++---+---+---+---+---+---+---+
+|   |   |   |   |   |   |   |
++---+---+---+---+---+---+---+
+|   |   |   |   |   |   |   |
++---+---+---+---+---+---+---+
+|   |   |   |   |   |   |   |
++---+---+---+---+---+---+---+
+|   | X | O |   |   |   |   |
++---+---+---+---+---+---+---+
+| X | O | X |   |   |   |   |
++---+---+---+---+---+---+---+
+
+*/
